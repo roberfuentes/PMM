bf4b8b6b5ddc707f67acfba0996ba069c63d5f7a
roberfuentes <roberfuentes@users.noreply.github.com>
2019-11-13 06:56:05 +0100

    Ejercicio recopilatorio hecho

---
+++ ExamenTest/app/src/main/java/com/example/examentest/SelectedPizza.java
@@ -0,0 +1,16 @@
+package com.example.examentest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectedPizza extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selected_pizza);
+    }
+}
